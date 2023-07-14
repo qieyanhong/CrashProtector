5d58a8077388c875dadeb5bc6dd25e2133e68ced
qieyanhong <qieyanhong@xiaozhu.com>
2023-07-14 15:42:07 +0800

    cologger

--- CrashProtector/Classes/CPLogger.h
+++ CrashProtector/Classes/CPLogger.h
@@ -11,15 +11,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CPLogger : NSObject
 
-+ (instancetype)logger;
-
-/// 设置上报日志的桥（可借助atos解析堆栈）
 + (void)setReportBridge:(void(^)(NSException *exception, NSArray *callStackSymbols, NSString *slideAddress))bridge;
 
-/// 上报日志
 + (void)reportException:(NSException *)exception callStackSymbols:(NSArray *)callStackSymbols;
 
 + (BOOL)enabled;
+
++ (instancetype)logger;
 
 @end
 
